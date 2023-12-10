05e340f23b1429f7f1ca3f07eb592b985dc3d38b
Cakeday <caleb.reyes.webdev@gmail.com>
2023-12-10 09:08:09 -0800

    page 68

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use zero2prod::run;
+use zero2prod::{startup::run, configuration::get_configuration};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("localhost:0").expect("Failed to bind to socket address.");
+    // Panic if we can't get our configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind to socket address.");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,13 +2,14 @@
 // It also spares you from having to specify the `#[test]` attr
 
 // To inspect, run `cargo expand --test health_check`
-
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("localhost:0").expect("Failed to bind to random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
 
     format!("http://localhost:{port}")
@@ -61,4 +62,40 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             "The API did not fail with a 400 Bad Request when the payload was {error_message}"
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read config");
+    let connection_string = configuration.database.connection_string();
+
+    // The 'Connection' trait MUST be in scope for us to invoke PgConnection::connect
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{app_address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    println!("{:?}", connection);
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }

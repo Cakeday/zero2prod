a6236f5e4a81cf30e42f65bb1851a367742040cf
Cakeday <caleb.reyes.webdev@gmail.com>
2024-01-14 00:18:27 -0800

    page 143

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // Panic if we can't get our configuration
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to postgres");
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
     let listener = TcpListener::bind(address).expect("Failed to bind to socket address.");
     run(listener, connection_pool)?.await
